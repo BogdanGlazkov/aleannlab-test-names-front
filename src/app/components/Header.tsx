7e580e64f21e4703988af7bc1512d0ac728ff716
BogdanGlazkov <bglazkov@i.ua>
2023-07-02 15:38:43 +0300

    Add header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-10 h-20 bg-teal-700 text-white p-4 border-b">
+      <a href="/">
+        <h1 className="text-3xl font-medium">Name Rank App</h1>
+      </a>
+    </header>
+  );
+};
+
+export default Header;
