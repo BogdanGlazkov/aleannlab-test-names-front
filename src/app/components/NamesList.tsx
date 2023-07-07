088489400b3a5be0ade76d787046b75931a1680f
BogdanGlazkov <bglazkov@i.ua>
2023-07-07 19:39:41 +0300

    Refactor NamesList state

--- src/app/components/NamesList.tsx
+++ src/app/components/NamesList.tsx
@@ -9,44 +9,52 @@ import Loader from "./Loader";
 import { NameDto } from "../api/dto/names.dto";
 
 const NamesList = () => {
-  const [fetchedNames, setFetchedNames] = useState<NameDto[] | null>(null);
-  const [editedNames, setEditedNames] = useState<NameDto[] | null>(null);
+  const [names, setNames] = useState<NameDto[] | null>(null);
   const [isRankChanged, setIsRankChanged] = useState(false);
-  const listToCompare = useRef<NameDto[] | null>(null);
+  const originalData = useRef<NameDto[] | null>(null);
 
   const onRankUp = (rank: number) => {
-    if (!editedNames) return;
-    if (rank <= 1) return;
-    setIsRankChanged(true);
-    const idx = editedNames.findIndex((item: NameDto) => item.rank === rank);
-    const editedArr = [...editedNames];
-    editedArr[idx].rank--;
-    editedArr[idx - 1].rank++;
-    setEditedNames(editedArr);
+    if (!names || rank <= 1) return;
+    setNames((prevNames) => {
+      if (!prevNames) return null;
+      const idx = prevNames.findIndex((item: NameDto) => item.rank === rank);
+      if (idx === -1 || !prevNames[idx - 1]) return prevNames;
+      const updatedNames = [...prevNames];
+      updatedNames[idx].rank--;
+      updatedNames[idx - 1].rank++;
+      setIsRankChanged(true);
+      return updatedNames;
+    });
   };
 
   const onRankDown = (rank: number) => {
-    if (!editedNames) return;
-    if (rank >= editedNames.length) return;
-    setIsRankChanged(true);
-    const idx = editedNames.findIndex((item: NameDto) => item.rank === rank);
-    const editedArr = [...editedNames];
-    editedArr[idx].rank++;
-    editedArr[idx + 1].rank--;
-    setEditedNames(editedArr);
+    if (!names || rank >= names.length) return;
+    setNames((prevNames) => {
+      if (!prevNames) return null;
+      const idx = prevNames.findIndex((item: NameDto) => item.rank === rank);
+      if (idx === -1) return prevNames;
+      const updatedNames = [...prevNames];
+      updatedNames[idx].rank++;
+      updatedNames[idx + 1].rank--;
+      setIsRankChanged(true);
+      return updatedNames;
+    });
   };
 
-  const onRankChangeSave = () => {
-    if (!editedNames || !listToCompare.current) return;
-    listToCompare.current.forEach(async (item) => {
-      const itemToCompare = editedNames.find((el) => el.id === item.id);
+  const onRankChangeSave = async () => {
+    if (!names || !originalData.current) return;
+
+    for (const item of originalData.current) {
+      const itemToCompare = names.find((el) => el.id === item.id);
+
       if (
-        item.rank !== itemToCompare?.rank &&
-        Number(itemToCompare?.rank) > 0
+        itemToCompare &&
+        +item.rank !== +itemToCompare.rank &&
+        +itemToCompare.rank > 0
       ) {
-        await editRank(item.id, Number(itemToCompare?.rank));
+        await editRank(item.id, +itemToCompare.rank);
       }
-    });
+    }
     setIsRankChanged(false);
   };
 
@@ -54,68 +62,71 @@ const NamesList = () => {
     const fetchData = async () => {
       const response = await getNames();
       if (response) {
-        setFetchedNames(response);
-        listToCompare.current = response.map((el: NameDto) => ({ ...el }));
+        setNames(response);
+        originalData.current = response.map((el: NameDto) => ({ ...el }));
       }
     };
     fetchData();
   }, []);
 
   useEffect(() => {
-    if (Array.isArray(fetchedNames))
-      setEditedNames(
-        fetchedNames.sort((a: NameDto, b: NameDto) => a.rank - b.rank)
+    if (Array.isArray(names)) {
+      const sortedNames = [...names].sort(
+        (a: NameDto, b: NameDto) => a.rank - b.rank
       );
-  }, [fetchedNames, editedNames]);
+      if (JSON.stringify(sortedNames) !== JSON.stringify(names)) {
+        setNames(sortedNames);
+      }
+    }
+  }, [names]);
 
-  if (!fetchedNames) return <Loader />;
-  else
-    return (
-      <div>
-        <h2 className="text-center text-3xl mb-4">Rank List</h2>
-        <ul>
-          {Array.isArray(editedNames) &&
-            editedNames.map((item: NameDto) => (
-              <li key={item.id} className="flex gap-2 my-1">
-                <div className="flex gap-2 mr-2">
-                  <button
-                    className="rounded hover:outline hover:outline-1"
-                    type="button"
-                    onClick={() => onRankUp(item.rank)}
-                    title="Up"
-                  >
-                    <AiOutlineUp fill="green" />
-                  </button>
-                  <button
-                    className="rounded hover:outline hover:outline-1"
-                    type="button"
-                    onClick={() => onRankDown(item.rank)}
-                    title="Down"
-                  >
-                    <AiOutlineDown fill="red" />
-                  </button>
-                </div>
+  if (!names) return <Loader />;
 
-                <NamesItem id={item.id} name={item.name} rank={item.rank} />
-              </li>
-            ))}
-        </ul>
+  return (
+    <div>
+      <h2 className="text-center text-3xl mb-4">Rank List</h2>
+      <ul>
+        {names.map((item: NameDto) => (
+          <li key={item.id} className="flex gap-2 my-1">
+            <div className="flex gap-2 mr-2">
+              <button
+                className="rounded hover:outline hover:outline-1"
+                type="button"
+                onClick={() => onRankUp(item.rank)}
+                title="Up"
+              >
+                <AiOutlineUp fill="green" />
+              </button>
+              <button
+                className="rounded hover:outline hover:outline-1"
+                type="button"
+                onClick={() => onRankDown(item.rank)}
+                title="Down"
+              >
+                <AiOutlineDown fill="red" />
+              </button>
+            </div>
 
-        {!isRankChanged ? null : (
-          <form className="mt-4" onSubmit={onRankChangeSave}>
-            <button
-              className="ml-2 rounded px-2 hover:outline hover:outline-1"
-              type="button"
-              title="Save"
-              onClick={onRankChangeSave}
-            >
-              Save changes
-            </button>
-          </form>
-        )}
-        <CreateForm />
-      </div>
-    );
+            <NamesItem id={item.id} name={item.name} rank={item.rank} />
+          </li>
+        ))}
+      </ul>
+
+      {!isRankChanged ? null : (
+        <form className="mt-4" onSubmit={onRankChangeSave}>
+          <button
+            className="ml-2 rounded px-2 hover:outline hover:outline-1"
+            type="button"
+            title="Save"
+            onClick={onRankChangeSave}
+          >
+            Save changes
+          </button>
+        </form>
+      )}
+      <CreateForm />
+    </div>
+  );
 };
 
 export default NamesList;
